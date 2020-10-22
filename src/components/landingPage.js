b34fb9b45be2018f95e9ba3053b95f5eb25d9bbe
Gina Frankel <g.frankel22@gmail.com>
2020-10-22 16:17:57 +0100

    :recycle: Extract LandingPage from Home

---
+++ src/components/landingPage.js
@@ -0,0 +1,24 @@
+import React from "react"
+
+import cottonImage from "../../static/cotton.jpg"
+import tShirtImage from "../../static/t-shirt.jpg"
+
+export default function LandingPage() {
+  return (
+    <>
+      <h1 className="mb-5">Cotton</h1>
+      <div className="row">
+        <div className="col-lg-5 col-md-3">
+          <img
+            src={cottonImage}
+            className="img-fluid "
+            alt="cotton-field"
+          ></img>
+        </div>
+        <div className=" offset-lg-2 col-lg-5 col-md-3">
+          <img src={tShirtImage} className="img-fluid" alt="t-shirt"></img>
+        </div>
+      </div>
+    </>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,25 +1,13 @@
 import React from "react"
+
 import Layout from "../components/layout"
-import cottonImage from "../../static/cotton.jpg"
-import tShirtImage from "../../static/t-shirt.jpg"
+import LandingPage from "../components/landingPage"
 
 export default function Home() {
   return (
     <Layout>
       <div className="container">
-        <h1 className="mb-5">Cotton</h1>
-        <div className="row">
-          <div className="col-lg-5 col-md-3">
-            <img
-              src={cottonImage}
-              className="img-fluid"
-              alt="cotton-field"
-            ></img>
-          </div>
-          <div className=" offset-lg-2 col-lg-5 col-md-3">
-            <img src={tShirtImage} className="img-fluid" alt="t-shirt"></img>
-          </div>
-        </div>
+        <LandingPage></LandingPage>
       </div>
     </Layout>
   )
